b7977f03514922c1f1441171f68a7fb9e82d941f
Sumosit <erlan99isaev@gmail.com>
2021-02-24 12:07:22 +0600

    -jwt, +keycloak

--- src/main/java/com/example/kafka/test/Entity/User.java
+++ src/main/java/com/example/kafka/test/Entity/User.java
@@ -1,12 +1,14 @@
 package com.example.kafka.test.Entity;
 
-import java.util.HashSet;
-import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 
 @Entity
 @Table(	name = "users",
@@ -14,10 +16,15 @@ import javax.validation.constraints.Size;
         @UniqueConstraint(columnNames = "username"),
         @UniqueConstraint(columnNames = "email")
     })
-public class User {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  private static final long serialVersionUID = -1L;
 
   @NotBlank
   @Size(max = 20)
@@ -29,61 +36,17 @@ public class User {
   private String email;
 
   @NotBlank
+  @Size(max = 50)
+  @Email
+  private String firstName;
+
+  @NotBlank
+  @Size(max = 50)
+  @Email
+  private String lastName;
+
+  @NotBlank
   @Size(max = 120)
   private String password;
 
-  @ManyToMany(fetch = FetchType.LAZY)
-  @JoinTable(	name = "user_roles",
-      joinColumns = @JoinColumn(name = "user_id"),
-      inverseJoinColumns = @JoinColumn(name = "role_id"))
-  private Set<Role> roles = new HashSet<>();
-
-  public User() {
-  }
-
-  public User(String username, String email, String password) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public Set<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
-  }
 }
